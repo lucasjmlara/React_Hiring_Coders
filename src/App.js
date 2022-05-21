1fb98460eaa145033d2d96f6acdfcfc0dbaaf9f2
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-05-20 23:15:27 -0300

    Aula API github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}`).then(res => console.log(res.data));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input placeholder='Usuário' className='usuarioInput' value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type='button' onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
