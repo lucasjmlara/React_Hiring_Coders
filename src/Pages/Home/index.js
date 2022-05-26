eebd478f5891aa308397d23abe46a2530ac71124
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-05-25 23:29:27 -0300

    Aula pegar nome dos repositorios

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,9 +4,20 @@ import * as S from './styled';
 
 function Home(props) {
   const [usuario, setUsuario] = useState('');
+
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}`).then(res => console.log(res.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(res => {
+      const repositories = res.data;
+      const reposName = [];
+
+      repositories.map((repository => {
+        reposName.push(repository.name)
+      }))
+      
+      localStorage.setItem('repositoriesName', JSON.stringify(reposName));
+    });
   }
+
   return (
     <S.Container>
       <S.Input placeholder='Usuário' className='usuarioInput' value={usuario} onChange={e => setUsuario(e.target.value)}/>
